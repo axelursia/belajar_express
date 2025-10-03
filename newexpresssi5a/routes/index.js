f5d3fa8dfc4b63df2398c9d30960e54f25a2d557
Axel Theo Winata Usia <axeltheowinataursia_2226240117@mhs.mdp.ac.id>
2025-10-03 14:31:43 +0700

    update

--- newexpresssi5a/routes/index.js
+++ newexpresssi5a/routes/index.js
@@ -6,4 +6,24 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express", layout: "main" });
 });
 
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Contact Us", layout: "main" });
+});
+
+router.get("/aboyt", function (req, res, next) {
+  res.render("about", { title: "About Us", layout: "main" });
+});
+
+router.get("/prodi", function (req, res, next) {
+  // Data program studi
+  const prodi = [
+    { nama: "Teknik Elektro", singkatan: "TE", fakultas: "Fakultas Ilmu Komputer & Rekayasa" },
+    { nama: "Informatika", singkatan: "IF", fakultas: "Fakultas Ilmu Komputer & Rekayasa" },
+    { nama: "Sistem Informasi", singkatan: "SI", fakultas: "Fakultas Ilmu Komputer & Rekayasa" },
+    { nama: "Akuntansi", singkatan: "AK", fakultas: "Fakultas Ekonomi & Bisnis" },
+    { nama: "Manajemen", singkatan: "MJ", fakultas: "Fakultas Ekonomi & Bisnis" },
+  ];
+  res.render("prodi", { prodi, title: "Program Studi", layout: "main" });
+});
+
 module.exports = router;
